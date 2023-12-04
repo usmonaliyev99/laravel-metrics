f41878cae2d82609287da4e84523f26aaf259e56
Temur5319436 <temur5319436@gmail.com>
2023-12-04 19:27:07 +0500

    feat: config/metric.php config file is added

---
+++ config/metric.php
@@ -0,0 +1,52 @@
+<?php
+
+return [
+    /**
+     * To enable or disable the metrics
+     */
+    'enable' => env('METRIC_ENABLE', false),
+
+    /**
+     * Redis connection name
+     */
+    'connection' => env('METRIC_CONNECTION', 'metric'),
+
+    /**
+     * List of connections
+     */
+    'redis' => [
+
+        'metric' => [
+            'url' => env('METRIC_REDIS_URL'),
+            'host' => env('METRIC_REDIS_HOST', '127.0.0.1'),
+            'username' => env('METRIC_REDIS_USERNAME'),
+            'password' => env('METRIC_REDIS_PASSWORD'),
+            'port' => env('METRIC_REDIS_PORT', '6379'),
+            'database' => env('METRIC_REDIS_CACHE_DB', '3'),
+        ]
+    ],
+
+    /**
+     * The configuration to enable or disable handlers
+     */
+    'control' => [
+
+        'query_counter' => env('METRIC_QUERY_COUNT', true),
+
+        'query_speed' => env('METRIC_QUERY_SPEED', true),
+    ],
+
+    /**
+     * The prefix for redis keys
+     */
+    'prefix' => [
+
+        'query' => [
+
+            'count' => 'metrics:query-counts',
+
+            'speed' => 'metrics:query-speeds',
+        ]
+    ]
+
+];
