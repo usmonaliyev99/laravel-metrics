f49a8650c409fb348e23fbe2a261ef75d7815a90
Temur5319436 <temur5319436@gmail.com>
2023-12-11 16:28:41 +0500

    feat(LaravelMetricServiceProvider.php): schedule is added and NotifyReportCommand is registered.

--- src/LaravelMetricServiceProvider.php
+++ src/LaravelMetricServiceProvider.php
@@ -2,10 +2,12 @@
 
 namespace Usmonaliyev\LaravelMetrics;
 
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Database\Events\QueryExecuted;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
+use Usmonaliyev\LaravelMetrics\Console\Commands\NotifyReportCommand;
 use Usmonaliyev\LaravelMetrics\Listeners\QueryListener;
 
 class LaravelMetricServiceProvider extends ServiceProvider
@@ -31,8 +33,23 @@ class LaravelMetricServiceProvider extends ServiceProvider
         if ($this->app->runningInConsole()) {
 
             $this->publishes([__DIR__ . '/../config/metric.php' => config_path('metric.php')], 'config');
+
+            $this->commands([
+                NotifyReportCommand::class,
+            ]);
         }
 
         Config::set('database.redis.metric', config('metric.redis.metric'));
+
+        /**
+         * Register schedule
+         */
+        $this->app->booted(function () {
+
+            $this->app
+                ->make(Schedule::class)
+                ->command('metric:notify-report')
+                ->cron(config('metric.notify.cron'));
+        });
     }
 }
